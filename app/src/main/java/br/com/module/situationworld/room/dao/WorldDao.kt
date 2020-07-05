c6d6380bbf21a08dfe9b632fe8f1cedc597e2dc4
Vinicius Rodrigues <rodriguesviniciusoliveira@gmail.com>
2020-07-05 18:45:20 -0300

    Dependencies.gradle center

--- app/src/main/java/br/com/module/situationworld/room/dao/WorldDao.kt
+++ app/src/main/java/br/com/module/situationworld/room/dao/WorldDao.kt
@@ -1,4 +1,18 @@
 package br.com.module.situationworld.room.dao
 
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import br.com.module.situationworld.room.entity.World
+
 interface WorldDao {
+
+    @Query("SELECT * FROM "+ World.TABLE_NAME)
+    fun getWorld(): List<World>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(world: World)
+
+    @Query("DELETE FROM "+ World.TABLE_NAME)
+    fun deleteAll()
 }
