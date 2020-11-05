391b142eac2247f0a905030541f35475f6471367
vinirod <rodriguesviniciusoliveira@gmail.com>
2020-11-05 16:26:03 -0300

    Ola mundo

--- app/src/main/java/br/com/module/situationworld/views/activities/LaunchScreenActivity.kt
+++ app/src/main/java/br/com/module/situationworld/views/activities/LaunchScreenActivity.kt
@@ -67,4 +67,6 @@ class LaunchScreenActivity : AppCompatActivity() {
     private fun setImageOnImageView() {
         GlideViewUtils.initGlidView(this, R.drawable.imgbg, mImgBackground)
     }
+
+    //Ola mundo
 }
